695823788d4293ccd1da55c78870fa2e2ba01b75
Carlos Souza <carloshrsouza@gmail.com>
2019-09-25 10:19:34 -0400

    Test reads from query string

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -3,11 +3,18 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"golang-fifa-world-cup-web-service/data"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"path"
 	"path/filepath"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
@@ -56,21 +63,37 @@ func TestListWinnersReturnsAllWinners(t *testing.T) {
 }
 
 func TestListWinnersReadsYearQueryString(t *testing.T) {
-	setup()
+	failingTestMessage := "Did not assign query string \"year\" to variable year"
+	defer func() {
+		if recover() != nil {
+			t.Error(failingTestMessage)
+		}
+	}()
+	hasAssignedToYear := false
+	isReadingStringArg := false
 
-	handler := http.HandlerFunc(ListWinners)
-	rr := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/winners", nil)
-	q := req.URL.Query()
-	q.Add("year", "1970")
-	req.URL.RawQuery = q.Encode()
-	handler.ServeHTTP(rr, req)
+	listWinnersFunc := getFuncDecl("ListWinners", "handlers.go")
+	funcBody := listWinnersFunc.Body
+	for _, funcStatement := range funcBody.List {
+		if reflect.TypeOf(funcStatement).String() == "*ast.AssignStmt" {
+			// Checks if `year` variable is being assigned to
+			assignment := funcStatement.(*ast.AssignStmt)
+			left := assignment.Lhs[0].(*ast.Ident)
+			hasAssignedToYear = (left.Name == "year")
 
-	body := rr.Body.String()
-	var winners data.Winners
-	json.Unmarshal([]byte(body), &winners)
-	if len(winners.Winners) == 21 {
-		t.Error("Did not read from query string")
+			// Checks if req.URL.Query().Get("year")
+			// is being called
+			right := assignment.Rhs[0].(*ast.CallExpr)
+			rightArg := right.Args[0].(*ast.BasicLit)
+			isReadingStringArg = (rightArg.Value == "\"year\"")
+
+			if hasAssignedToYear && isReadingStringArg {
+				break
+			}
+		}
+	}
+	if !hasAssignedToYear || !isReadingStringArg {
+		t.Error(failingTestMessage)
 	}
 }
 
@@ -222,4 +245,33 @@ func TestCorrectHTTPUnsupportedMethodDispatch(t *testing.T) {
 	if status := rr.Code; status != http.StatusMethodNotAllowed {
 		t.Error("Did not properly catch unsupported HTTP methods", status)
 	}
+}
+
+func getFuncDecl(name string, filename string) *ast.FuncDecl {
+	var found *ast.FuncDecl
+
+	_, currentFile, _, _ := runtime.Caller(1)
+
+	src, err := ioutil.ReadFile(path.Join(path.Dir(currentFile), filename))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "", src, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, decl := range f.Decls {
+		if reflect.TypeOf(decl).String() == "*ast.FuncDecl" {
+			funDecl := decl.(*ast.FuncDecl)
+			if funDecl.Name.String() == name {
+				found = funDecl
+				break
+			}
+		}
+	}
+	//ast.Print(fset, found)
+	return found
 }
