673fa5aae13458e94f20ef05c4bfd2c7030826a8
Zvika Ekhous <zvoykish@gmail.com>
2022-11-09 20:43:20 -0800

    Creating the dispatch handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -54,5 +54,12 @@ func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 
 // WinnersHandler is the dispatcher for all /winners URL
 func WinnersHandler(res http.ResponseWriter, req *http.Request) {
-
+	switch req.Method {
+	case http.MethodGet:
+		ListWinners(res, req)
+	case http.MethodPost:
+		AddNewWinner(res, req)
+	default:
+		res.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
