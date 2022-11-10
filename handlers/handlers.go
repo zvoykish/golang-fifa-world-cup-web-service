42ebd3c64b8e24dffd62679a1ede3e70bcc4ed65
Zvika Ekhous <zvoykish@gmail.com>
2022-11-09 20:37:50 -0800

    Creating POST handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -35,6 +35,20 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	isTokenValid := data.IsAccessTokenValid(accessToken)
+	if !isTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	err := data.AddNewWinner(req.Body)
+	if err != nil {
+		res.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+
+	res.WriteHeader(http.StatusCreated)
 
 }
 
